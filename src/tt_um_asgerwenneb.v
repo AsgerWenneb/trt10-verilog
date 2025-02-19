9ff5a8278ddd141ff7074ef5fd24f48e05ea3ba2
AsgerWenneb <asger@wenneberg.net>
2025-02-19 14:27:59 +0100

    counter

--- src/tt_um_asgerwenneb.v
+++ src/tt_um_asgerwenneb.v
@@ -15,15 +15,19 @@ module tt_um_asgerwenneb (
     input  logic       clk,      // clock
     input  logic       rst_n     // reset_n - low to reset
 );
+    logic [7:0] count;
 
-    // All output pins must be assigned. If not used, assign to 0.
-    assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    always_ff @(posedge clk) begin
+        if (!rst_n)
+            count <= 0;
+        else
+            count <= count + 1;
+    end
+
+    assign uo_out  = count;
     assign uio_out = 0;
     assign uio_oe  = 0;
 
-
-
     // List all unused inputs to prevent warnings
     wire _unused = &{ena, clk, rst_n, 1'b0};
-
 endmodule
