73d1bb073738f106a844cc7f48714abca572d140
AsgerWenneb <asger@wenneberg.net>
2025-03-12 20:14:08 +0100

    update to 1x2

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_awenneb_sram (
+module tt_um_asgerwenneb (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
